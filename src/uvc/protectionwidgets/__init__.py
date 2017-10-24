c321fb6f1b4e04338f47c44566b88eb4fb688bb9
goschtl <ck@novareto.de>
2017-10-24 09:54:04 +0200

    chagnes

--- src/uvc/protectionwidgets/__init__.py
+++ src/uvc/protectionwidgets/__init__.py
@@ -2,4 +2,9 @@
 # Copyright (c) 2008-2013 Infrae. All rights reserved.
 # See also LICENSE.txt
 
+from zope.i18nmessageid import MessageFactory
+MF = MessageFactory('uvc.protectionwidgets')
+
 from .fields import Captcha
+from .mixins import CSRFMixin
+
